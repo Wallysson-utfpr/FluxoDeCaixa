835ef82a814a1dfc533f7696e07e9c4108d71f87
Wallysson Barbosa <73896874+Wallysson-utfpr@users.noreply.github.com>
2022-08-23 22:28:50 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, ImageBackground } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/imgbackground.jpg')} style={styles.imageBackground}>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, //ocupa toda a parte da tela
+    flexDirection: 'column',
+    backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  imageBackground: {
+    flex: 1,
+    justifyContent: 'center',
+    resizeMode: 'cover',
+    alignItems: 'center',
+    width: "100%",
+  },
+
 });
